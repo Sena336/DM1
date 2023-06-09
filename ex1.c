b62318b23c4038d4932b4e72360d9674da06d17d
Sena336 <136040443+Sena336@users.noreply.github.com>
2023-06-09 12:20:25 +0900

    Hot: Initial revision

---
+++ ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int rollDice() {
+    return rand() % 6 + 1;
+}
+
+int main() {
+    int die1, die2, totalValue;
+
+    printf("Rolling dice...\n");
+    srand(time(NULL));
+
+    die1 = rollDice();
+    die2 = rollDice();
+    totalValue = die1 + die2;
+
+    printf("Die 1: %d\n", die1);
+    printf("Die 2: %d\n", die2);
+    printf("Total value: %d\n", totalValue);
+
+    return 0;
+}
